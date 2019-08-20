73fdaec1b4c726a1c4986891744eff96fd33f939
lilyhoratio <lilyhzhou@gmail.com>
2019-08-20 12:14:31 -0700

    controls tests with mock fns

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -5,7 +5,6 @@
 // the closed toggle button is disabled if the gate is locked
 // the locked toggle button is disabled if the gate is open
 
-
 import React from 'react'
 import { render, cleanup, fireEvent } from '@testing-library/react'
 // import '@testing-library/react/cleanup-after-each' 
@@ -29,4 +28,55 @@ describe("<Controls />", () => {
     it("close gate button text changes on click", () => {
         const wrapper = render(<Controls />)
     })
+
+    it("unlocked and open", () => {
+        const lockSpy = jest.fn()
+        const closeSpy = jest.fn()
+
+        const { getByText } = render(<Controls
+            closed={false}
+            locked={false}
+            toggleClosed={closeSpy}
+            toggleLocked={lockSpy}
+        />)
+        const closeButton = getByText(/close gate/i)
+        const lockButton = getByText(/lock gate/i)
+
+        // determine the button status 
+        expect(closeButton.disabled).toBeFalsy()
+        expect(lockButton.disabled).toBeTruthy()
+
+        // button click events
+        fireEvent.click(closeButton)
+        expect(closeSpy).toBeCalled()
+
+        fireEvent.click(lockButton)
+        expect(lockSpy).not.toBeCalled() // NOT called b/c is disabled
+    })
+
+    it("unlocked and closed", () => {
+        const lockSpy = jest.fn()
+        const closeSpy = jest.fn()
+
+        const { getByText } = render(<Controls
+            closed={true}
+            locked={false}
+            toggleClosed={closeSpy}
+            toggleLocked={lockSpy}
+        />)
+        const closeButton = getByText(/open gate/i)
+        const lockButton = getByText(/lock gate/i)
+
+        // determine the button status 
+        expect(closeButton.disabled).toBeFalsy()
+        expect(lockButton.disabled).toBeFalsy() //changes
+
+        // button click events
+        fireEvent.click(closeButton)
+        expect(closeSpy).toBeCalled()
+
+        fireEvent.click(lockButton)
+        expect(lockSpy).toBeCalled()
+    })
+
 })
